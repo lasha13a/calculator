db650aa5e851b5200d6eb513fc2bd8b9d4fda4fd
lasha13a <151869488+lasha13a@users.noreply.github.com>
2023-11-23 22:00:31 +0400

    Add files via upload

---
+++ calculator.php
@@ -0,0 +1,4 @@
+<?php
+// This is just a placeholder for the PHP file.
+// You can add server-side logic here if needed.
+?>
---
+++ script.js
@@ -0,0 +1,14 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    const displayValue = document.getElementById('display').value;
+    const result = eval(displayValue);
+
+    document.getElementById('display').value = result;
+}
